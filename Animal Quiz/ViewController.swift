0342b0fcb9345c389c737a784443321a8d978c2e
Danila Nagornyi <excelrin@gmail.com>
2020-04-25 18:55:35 +0300

    Initial Commit

---
+++ Animal Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animal Quiz
+//
+//  Created by Danila Nagornyi on 25.04.2020.
+//  Copyright © 2020 Danila Nagornyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
